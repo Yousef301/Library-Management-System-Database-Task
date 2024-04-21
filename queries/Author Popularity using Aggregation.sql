ee70f5e7d8f5e99816fe254b8d0c1a547b5f7df5
Yousef Shamasneh <102422691+Yousef301@users.noreply.github.com>
2024-04-21 19:31:16 +0300

    Delete queries directory

--- queries/Author Popularity using Aggregation.sql
+++
@@ -1,8 +0,0 @@
-select 
-  Author,
-  count(*) as BorrowingCount,
-  DENSE_RANK() over (order by count(*) desc) as AuthorRank
-from Loans l
-inner join Books b on l.BookID = b.BookID
-group by Author
-order by BorrowingCount desc;
--- queries/List of Borrowed Books.sql
+++
@@ -1,3 +0,0 @@
-select b.* from Books as b
-right join Loans as l on b.BookID = l.BookID
-where l.BorrowerID = 2
